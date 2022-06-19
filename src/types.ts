322486cdae465f1945155e79e47e2ce3b5157d2a
kumankoishi.py <7iggif@naver.com>
2022-06-20 05:39:48 +0900

    Added types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,13 @@
 export type AcceptsNumberOrString = number | string;
+
+export enum KonvaShapeKind {
+  Ellipse = 'KONVA_ELLIPSE',
+  Rect = 'KONVA_RECT',
+  Star = 'KONVA_STAR',
+};
+export type KonvaCommonShapeProps = {
+  fill?: string;
+  stroke?: string;
+  strokeWidth?: number;
+  draggable?: boolean;
+};
